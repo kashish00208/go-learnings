1293fbd98a7fce5a3862ba9f49b45716a7e14cb2
Kashish <kgupta7827@gmail.com>
2026-01-28 17:22:43 +0530

    flag , time and os/user library function

--- basics/ws.go
+++ basics/ws.go
@@ -6,7 +6,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/websocket"
-)s
+)
 
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
--- main.go
+++ main.go
@@ -1,3 +1,45 @@
 package main
 
-func main()
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os/user"
+	"time"
+)
+
+func flagLib() {
+	serverPort := flag.Int("port", 8080, "The port to run the server on")
+	env := flag.String("env", "dev", "The environment (dev/prod)")
+
+	flag.Parse()
+
+	fmt.Printf("Starting server on port %d in %s mode\n", *serverPort, *env)
+
+}
+
+func timeLib() {
+	//This library provides functionality for measuring and displaying time
+	//Allows us to find current time , format dates , sleep or pause
+
+	start := time.Now()
+	fmt.Println("The current time is ", start)
+}
+
+func osLib() {
+	//This library helps us to look upto user account information used to know who is running the current program
+
+	currentUser, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Username:", currentUser.Username)
+	fmt.Println("Home Directory:", currentUser.HomeDir)
+	fmt.Println("User ID (UID):", currentUser.Uid)
+
+}
+
+func main() {
+
+}
