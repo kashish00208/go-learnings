8791d1180c3b72e3d9bd819d8c90864b96c2f1f2
Kashish <kgupta7827@gmail.com>
2026-03-01 01:11:23 +0530

    hello world

--- main.go
+++ main.go
@@ -1,45 +1,7 @@
-package main
+package main 
 
 import "fmt"
 
-type Operation struct {
-	A      int
-	B      int
-	Result int
-	Op     string
-}
-
-var history []Operation
-
-func calculate(a, b int, op string) (int, error) {
-	var res int
-
-	switch op {
-	case "+":
-		res = a + b
-	case "-":
-		res = a - b
-	case "*":
-		res = a * b
-	case "/":
-		res = a / b
-	default:
-		return 0, fmt.Errorf("Unknown Operator")
-	}
-
-	history = append(history, Operation{a, b, res, op})
-	return res, nil
-}
-func showHistory() {
-	fmt.Println("Operation History:")
-	for i, op := range history {
-		fmt.Printf("%d. %d %s %d = %d\n", i+1, op.A, op.Op, op.B, op.Result)
-	}
-}
-
-func main() {
-	calculate(10, 5, "+")
-	calculate(20, 4, "-")
-	calculate(3, 7, "*")
-	showHistory()
+func main()  {
+	fmt.Println("hello world")
 }
